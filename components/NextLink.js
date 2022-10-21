3553ab87aca712d3d76d49060759e1e472e063b5
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-21 16:19:01 +0600

    🛠️🏷️📕converted to bootstrap component

--- components/NextLink.js
+++ components/NextLink.js
@@ -1,14 +1,21 @@
-import { Link as MuiLink } from "@mui/material";
-import Link from "next/link";
 import React, { forwardRef } from "react";
 
-const NextLink = forwardRef(({ href, as, prefetch, ...props }, ref) => {
-  return (
-    <Link href={href} as={as} prefetch={prefetch} passHref>
-      <MuiLink ref={ref} {...props} />
-    </Link>
-  );
-});
+const NextLink = forwardRef(
+  ({ href, as, prefetch, children, className, ...props }, ref) => {
+    return (
+      <a
+        className={className}
+        href={href}
+        as={as}
+        prefetch={prefetch || ""}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </a>
+    );
+  }
+);
 
 NextLink.displayName = "NextLink";
 
